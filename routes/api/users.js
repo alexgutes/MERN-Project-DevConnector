25c31e71125e094f5115e2eff7951f919733b241
Alex <0alexgutierrez@gmail.com>
2019-04-08 16:26:00 -0700

    setup POST /register

--- routes/api/users.js
+++ routes/api/users.js
@@ -43,4 +43,25 @@ router.post('/register', (req, res) => {
   });
 });
 
+// @route GET api/users/login
+// @desc  Login user and return JWT
+// @access Public
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  // find user by email
+  User.findOne({ email }).then((user) => {
+    if (!user) {
+      return res.status(404).json({ email: 'user not found' });
+    }
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        res.json({ msg: 'success' });
+      } else {
+        return res.status(400).json({ password: 'password incorrect' });
+      }
+    });
+  });
+});
+
 module.exports = router;
